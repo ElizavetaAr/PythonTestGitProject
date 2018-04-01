105bfaf9fef218921a9b175086a432b4816a8821
ElizavetaAr <arkusha.liza@yandex.ru>
2018-04-02 02:39:06 +0300

    Add system info output

--- start.py
+++ start.py
@@ -1,1 +1,9 @@
+import platform
+
 print('PythonTestGitProject')
+
+print(plarform.system())
+print(platform.platform())
+print(platform.machine())
+print(platform.version())
+print(platform.processor())
